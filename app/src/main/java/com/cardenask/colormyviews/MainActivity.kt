59cbc4e7212b467f560bb411bab948c33dfa6422
Kevin Cardenas <kcard13@terpmail.umd.edu>
2019-07-02 16:32:44 -0400

    Added the click listeners. Changes colors of the TextViews and the ConstraintLayout

--- app/src/main/java/com/cardenask/colormyviews/MainActivity.kt
+++ app/src/main/java/com/cardenask/colormyviews/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.cardenask.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+
+        val clickableViews : List<View> = listOf(box_one_text, box_two_text, box_three_text,
+                                                 box_four_text, box_five_text, constraint_layout)
+
+        for (item in clickableViews) {
+            item.setOnClickListener {
+                makeColored(it)
+            }
+        }
+    }
+
+    private fun makeColored(view: View) {
+
+        when (view.id) {
+
+            box_one_text.id -> view.setBackgroundColor(Color.DKGRAY)
+            box_two_text.id -> view.setBackgroundColor(Color.GRAY)
+            box_three_text.id -> view.setBackgroundResource(android.R.color.holo_green_light)
+            box_four_text.id -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            box_five_text.id -> view.setBackgroundResource(android.R.color.holo_red_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
